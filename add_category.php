6152a307b985f1e7def892a345bc5a25d6ea43d1
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-27 01:03:58 +0600

    Store_ms

--- add_category.php
+++ add_category.php
@@ -9,9 +9,6 @@ $conn = new mysqli($hostname, $username, $password, $dbname );
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-  
-
 ?>
 
 <!DOCTYPE html>
---
+++ list_of_category.php
@@ -0,0 +1,34 @@
+<?php
+require('connection.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List of Category</title>
+    <style>
+        table, th, td {
+        border: 1px solid;
+        }
+        </style>
+</head>
+<body>
+    <?php
+       $sql =  "SELECT* FROM category" ;
+
+       $query = $conn->query($sql);
+
+    echo "<table><tr> <th> Category </th> <th> Date </th>  <th> Action </th></tr> ";
+
+     while ($data = mysqli_fetch_assoc($query)) {
+        $category_name = $data['Category_name'];
+        $category_entrydate = $data['Category_entrydate'];
+
+   echo "<tr><td> $category_name</td><td> $category_entrydate </td><td>Edit</d></tr>";
+    }
+        echo "</table>"
+    ?>
+</body>
+</html>
